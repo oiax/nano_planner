49d184572b6e2394c213d55ccd5ebb901df66db2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-10-22 12:35:05 +0900

    Remove api stack

--- web/router.ex
+++ web/router.ex
@@ -9,18 +9,9 @@ defmodule NanoPlanner.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", NanoPlanner do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", NanoPlanner do
-  #   pipe_through :api
-  # end
 end
