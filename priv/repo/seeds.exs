3b3e8743ebc5efde9e077c65949dd0daf2a277b9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-10-23 10:17:51 +0900

    Create seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,7 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     NanoPlanner.Repo.insert!(%NanoPlanner.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+import NanoPlanner.Repo, only: [insert!: 1]
+alias NanoPlanner.PlanItem
+
+insert! %PlanItem{
+  name: "読書",
+  description: "『走れメロス』を読む"
+}
