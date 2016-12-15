180851516d0e0546ae911c11c148fc563ebf510c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-15 21:04:52 +0900

    Add more seed records

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,3 +5,13 @@ insert! %PlanItem{
   name: "読書",
   description: "『走れメロス』を読む"
 }
+
+insert! %PlanItem{
+  name: "買い物",
+  description: "洗剤を買う"
+}
+
+insert! %PlanItem{
+  name: "帰省",
+  description: "新幹線の指定席を取る。\nお土産を買う。"
+}
