7dc585a8dc9506455fc4bf61600490033a7e8663
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-17 12:49:25 +0900

    Refactor Mix.Tasks.DB.Count module

--- lib/mix/tasks/db.count.ex
+++ lib/mix/tasks/db.count.ex
@@ -1,17 +1,12 @@
 defmodule Mix.Tasks.Db.Count do
   use Mix.Task
-  import Mix.Ecto
 
   @shortdoc "Show the record count of a database table"
 
   def run(args) do
-    [repo|_] = parse_repo([])
-    ensure_repo(repo, args)
-    ensure_started(repo, [])
+    repo = Mix.Util.prepare_repo()
+    mod = List.first(args) |> Mix.Util.model_module()
 
-    app_name = Mix.Project.config[:app] |> Atom.to_string() |> Macro.camelize()
-    model_name = List.first(args) |> Macro.camelize()
-
-    IO.puts repo.aggregate(Module.concat(app_name, model_name), :count, :id)
+    IO.puts repo.aggregate(mod, :count, :id)
   end
 end
---
+++ lib/mix/util.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Util do
+  import Mix.Ecto
+
+  def prepare_repo() do
+    [repo|_] = parse_repo([])
+    ensure_repo(repo, [])
+    ensure_started(repo, [])
+    repo
+  end
+
+  def model_module(name) do
+    app_name = Mix.Project.config[:app] |> Atom.to_string() |> Macro.camelize()
+    model_name = Macro.camelize(name)
+
+    Module.concat(app_name, model_name)
+  end
+end
