2b401186c6b01eb1a528068388e74db11493afd8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-17 12:50:52 +0900

    Remove unnecessary lines from Mix.Util module

--- lib/mix/util.ex
+++ lib/mix/util.ex
@@ -3,7 +3,6 @@ defmodule Mix.Util do
 
   def prepare_repo() do
     [repo|_] = parse_repo([])
-    ensure_repo(repo, [])
     ensure_started(repo, [])
     repo
   end
@@ -11,7 +10,6 @@ defmodule Mix.Util do
   def model_module(name) do
     app_name = Mix.Project.config[:app] |> Atom.to_string() |> Macro.camelize()
     model_name = Macro.camelize(name)
-
     Module.concat(app_name, model_name)
   end
 end
