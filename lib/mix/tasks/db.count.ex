e9c5eb0a779529a8259ee2908ed4c446832ab3e3
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-17 15:01:38 +0900

    Create db.count mix task

---
+++ lib/mix/tasks/db.count.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Db.Count do
+  use Mix.Task
+
+  @shortdoc "Show the record count of a database table"
+
+  def run(args) do
+    repo = Mix.Util.prepare_repo()
+    mod = List.first(args) |> Mix.Util.model_module()
+
+    IO.puts repo.aggregate(mod, :count, :id)
+  end
+end
