2bfbc9518dc7978fa19074b709ec493957994ff8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-18 13:54:37 +0900

    Introduce camshaft/html_builder

--- web/views/top_view.ex
+++ web/views/top_view.ex
@@ -1,3 +1,12 @@
 defmodule NanoPlanner.TopView do
   use NanoPlanner.Web, :view
+
+  def foo do
+    {"div", [{:class, "x 1"}, {:"data-foo", "y"}], [
+      {"span", nil, "Hello"},
+      {"br"},
+      {"br"},
+      {"span", nil, "World!"}
+    ]} |> HTMLBuilder.encode!(pretty: true)
+  end
 end
