1733b64c9cc9f3629dcb6da79a6a26972d8586b7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-19 00:04:47 +0900

    plan_items#index: 実装

--- web/controllers/plan_items_controller.ex
+++ web/controllers/plan_items_controller.ex
@@ -1,7 +1,8 @@
 defmodule NanoPlanner.PlanItemsController do
   use NanoPlanner.Web, :controller
+  alias NanoPlanner.PlanItem
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", plan_items: Repo.all(PlanItem)
   end
 end
