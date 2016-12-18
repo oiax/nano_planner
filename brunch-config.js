5d1feb9fa6d5f9180f52a5eef5f65768707eee85
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-18 23:41:14 +0900

    Font Awesomeの導入（し忘れ）

--- brunch-config.js
+++ brunch-config.js
@@ -55,8 +55,8 @@ exports.config = {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
     },
-    sass: {
-      mode: "native"
+    copycat: {
+      fonts: ["node_modules/font-awesome/fonts"]
     }
   },
 
