0a39d3c404e9dbce9e74b2f858afc3020bc2ae5b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-18 11:50:27 +0900

    Try joshnuss/xml_builder

--- web/views/top_view.ex
+++ web/views/top_view.ex
@@ -1,3 +1,13 @@
 defmodule NanoPlanner.TopView do
   use NanoPlanner.Web, :view
+  import XmlBuilder, only: [generate: 1]
+
+  def foo do
+    {
+      :div, %{class: "foo"}, [
+        {:span, nil, "A" },
+        {:span, nil, "B" }
+      ]
+    } |> generate
+  end
 end
