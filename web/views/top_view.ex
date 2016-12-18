053691e6e56ec494e54f21324522983e86c262e6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-18 18:44:37 +0900

    Add more lines to foo()

--- web/views/top_view.ex
+++ web/views/top_view.ex
@@ -1,12 +1,20 @@
 defmodule NanoPlanner.TopView do
   use NanoPlanner.Web, :view
+  import HTMLBuilder, only: [encode!: 1]
 
-  def foo do
-    {"div", [{:class, "x 1"}, {:"data-foo", "y"}], [
-      {"span", nil, "Hello"},
-      {"br"},
-      {"br"},
-      {"span", nil, "World!"}
-    ]} |> HTMLBuilder.encode! |> raw
+  def foo(args) do
+    attr = []
+    attr = [{:"data-foo", "y"}|attr]
+    if true do
+      attr = [{:class, "btn btn-primary"}|attr]
+    end
+    {"div", attr, span_list(args)} |> encode! |> raw
+  end
+
+  defp span_list(args) do
+    [
+      {"div", [style: "color: red"], "Hello"},
+      {"div", nil, "World!"}
+    ] ++ Enum.map(args, fn(e) -> {"div", nil, e} end)
   end
 end
