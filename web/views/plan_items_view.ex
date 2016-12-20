187edf801d1e643a8dab309ed92170b054d4acbd
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-20 12:13:17 +0900

    日付と時刻の整形(1)

--- web/views/plan_items_view.ex
+++ web/views/plan_items_view.ex
@@ -1,3 +1,11 @@
 defmodule NanoPlanner.PlanItemsView do
   use NanoPlanner.Web, :view
+
+  def format_duration(item) do
+    [
+      item.starts_at,
+      "～",
+      item.ends_at
+    ] |> Enum.join
+  end
 end
