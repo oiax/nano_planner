cc569f4149235ebec929a2ce102f0b486c8d509c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-20 14:03:45 +0900

    日付と時刻の整形(2)

--- web/views/plan_items_view.ex
+++ web/views/plan_items_view.ex
@@ -1,11 +1,17 @@
 defmodule NanoPlanner.PlanItemsView do
   use NanoPlanner.Web, :view
+  import Timex.Format.DateTime.Formatters.Strftime, only: [format!: 2]
 
   def format_duration(item) do
     [
-      item.starts_at,
+      format_datetime(item.starts_at),
       "～",
-      item.ends_at
+      format_datetime(item.ends_at)
     ] |> Enum.join
+  end
+
+  defp format_datetime(time) do
+    time |> Timex.Timezone.convert("Asia/Tokyo")
+      |> format!("%Y年%-m月%-d日 %H:%M")
   end
 end
