eebd2e910dff1b0b5954542cedc65c239f55f7d1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-01-23 00:43:49 +0900

    Bump timex to 3.1; mix deps.update

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule NanoPlanner.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:timex, "~> 3.0"},
-     {:timex_ecto, "~> 3.0"}]
+     {:timex, "~> 3.1"},
+     {:timex_ecto, "~> 3.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
