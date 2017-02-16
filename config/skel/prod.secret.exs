24160788c81e9950b133afa69363219f4e7aa950
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-02-17 02:40:03 +0900

    Add config/skel/prod.secret.exs

---
+++ config/skel/prod.secret.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :nano_planner, NanoPlanner.Endpoint,
+  secret_key_base: "CHANGE_HERE"
+
+config :nano_planner, NanoPlanner.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "phoenix",
+  password: "phoenix",
+  database: "nano_planner_dev",
+  pool_size: 20
