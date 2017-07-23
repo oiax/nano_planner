b171808708b3f78833c055b79123af10ea95d281
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-07-23 16:59:47 +0900

    Show flash messages

--- lib/nano_planner/web/controllers/plan_item_controller.ex
+++ lib/nano_planner/web/controllers/plan_item_controller.ex
@@ -15,7 +15,9 @@ defmodule NanoPlanner.Web.PlanItemController do
 
   def create(conn, %{"plan_item" => plan_item_params}) do
     Calendar.create_plan_item(plan_item_params)
-    redirect(conn, to: plan_item_path(conn, :index))
+    conn
+    |> put_flash(:info, "予定を追加しました。")
+    |> redirect(to: plan_item_path(conn, :index))
   end
 
   def show(conn, %{"id" => id}) do
@@ -32,12 +34,16 @@ defmodule NanoPlanner.Web.PlanItemController do
   def update(conn, %{"id" => id, "plan_item" => plan_item_params}) do
     plan_item = Calendar.get_plan_item!(id)
     Calendar.update_plan_item(plan_item, plan_item_params)
-    redirect(conn, to: plan_item_path(conn, :index))
+    conn
+    |> put_flash(:info, "予定を変更しました。")
+    |> redirect(to: plan_item_path(conn, :index))
   end
 
   def delete(conn, %{"id" => id}) do
     plan_item = Calendar.get_plan_item!(id)
     Calendar.delete_plan_item(plan_item)
-    redirect(conn, to: plan_item_path(conn, :index))
+    conn
+    |> put_flash(:info, "予定を削除しました。")
+    |> redirect(to: plan_item_path(conn, :index))
   end
 end
