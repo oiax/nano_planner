4a61dc7dd30968515a7edba49d8662c17062f662
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-07-23 22:51:54 +0900

    Add select box for hours and minutes

--- lib/calendar/plan_item.ex
+++ lib/calendar/plan_item.ex
@@ -9,12 +9,23 @@ defmodule NanoPlanner.Calendar.PlanItem do
     field :description, :string, default: ""
     field :starts_at, Timex.Ecto.DateTime
     field :ends_at, Timex.Ecto.DateTime
+    field :s_date, Timex.Ecto.Date, virtual: true
+    field :s_hour, :integer, virtual: true
+    field :s_minute, :integer, virtual: true
+    field :e_date, Timex.Ecto.Date, virtual: true
+    field :e_hour, :integer, virtual: true
+    field :e_minute, :integer, virtual: true
 
     timestamps()
   end
 
+  @allowed_fields [
+    :name, :description,
+    :s_date, :s_hour, :s_minute,
+    :e_date, :e_hour, :e_minute
+  ]
   def changeset(%PlanItem{} = plan_item, attrs) do
     plan_item
-    |> cast(attrs, [:name, :description, :starts_at, :ends_at])
+    |> cast(attrs, @allowed_fields)
   end
 end
