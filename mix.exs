d95cdedd5df3be29e6dbd79a42861321eb38d7ad
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-11-25 16:36:56 +0900

    Downgrade ecto to 2.1.x

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule NanoPlanner.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:ecto, "~> 2.1.3"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"}]
   end
