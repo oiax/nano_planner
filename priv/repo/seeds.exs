0362eb40b8ab94d8420acd1cd7aea04a79298127
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-11-26 15:25:07 +0900

    volume02 ch06

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,17 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     NanoPlanner.Repo.insert!(%NanoPlanner.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+import NanoPlanner.Repo
+alias NanoPlanner.PlanItem
+
+insert! %PlanItem{
+  name: "読書",
+  description: "『走れメロス』を読む"
+}
+
+insert! %PlanItem{
+  name: "買い物",
+  description: "洗剤を買う"
+}
+
+insert! %PlanItem{
+  name: "帰省",
+  description: "新幹線の指定席を取る。お土産を買う。"
+}
