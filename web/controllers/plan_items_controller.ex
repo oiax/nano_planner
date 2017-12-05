67c4d6a79b9695d2c911900148bd57c82dc49d4f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-06 00:15:06 +0900

    volume02 ch11(1)

--- web/controllers/plan_items_controller.ex
+++ web/controllers/plan_items_controller.ex
@@ -3,7 +3,10 @@ defmodule NanoPlanner.PlanItemsController do
   alias NanoPlanner.PlanItem
 
   def index(conn, _params) do
-    plan_items = Repo.all(PlanItem)
+    plan_items =
+      PlanItem
+      |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
+      |> Repo.all
     render conn, "index.html", plan_items: plan_items
   end
 end
