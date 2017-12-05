48467c4aa045ef2abc963c5dd558406a136db8cc
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-06 00:33:06 +0900

    volume02 ch12

--- web/views/plan_items_view.ex
+++ web/views/plan_items_view.ex
@@ -1,3 +1,28 @@
 defmodule NanoPlanner.PlanItemsView do
   use NanoPlanner.Web, :view
+  alias Timex.Format.DateTime.Formatters.Strftime
+
+  def format_duration(item) do
+    [format_starts_at(item), "～", format_ends_at(item)] |> Enum.join(" ")
+  end
+
+  defp format_starts_at(item) do
+    time_zone = Application.get_env(:nano_planner, :default_time_zone)
+    if item.starts_at.year == Timex.now(time_zone).year do
+      Strftime.format! item.starts_at, "%-m月%-d日 %H:%M"
+    else
+      Strftime.format! item.starts_at, "%Y年%-m月%-d日 %H:%M"
+    end
+  end
+
+  defp format_ends_at(item) do
+    cond do
+      Timex.to_date(item.ends_at) == Timex.to_date(item.starts_at) ->
+        Strftime.format! item.ends_at, "%H:%M"
+      item.ends_at.year == item.starts_at.year ->
+        Strftime.format! item.ends_at, "%-m月%-d日 %H:%M"
+      true ->
+        Strftime.format! item.ends_at, "%Y年%-m月%-d日 %H:%M"
+    end
+  end
 end
