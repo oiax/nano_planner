ef3742a305965faa9cd0c919d2accc4c200ff290
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-10 00:49:40 +0900

    Refactor Calendar context

--- lib/nano_planner_web/controllers/plan_items_controller.ex
+++ lib/nano_planner_web/controllers/plan_items_controller.ex
@@ -1,24 +1,14 @@
 defmodule NanoPlannerWeb.PlanItemsController do
   use NanoPlannerWeb, :controller
-  import Ecto.Query
-  alias NanoPlanner.Repo
   alias NanoPlanner.Calendar
-  alias NanoPlanner.Calendar.PlanItem
 
   def index(conn, _params) do
-    plan_items =
-      PlanItem
-      |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
-      |> Repo.all
-      |> Calendar.convert_datetime
+    plan_items = Calendar.list_plan_items
     render conn, "index.html", plan_items: plan_items
   end
 
   def show(conn, params) do
-    plan_item =
-      PlanItem
-      |> Repo.get!(params["id"])
-      |> Calendar.convert_datetime
+    plan_item = Calendar.get_plan_item!(params["id"])
     render conn, "show.html", plan_item: plan_item
   end
 end
