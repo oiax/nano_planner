e382045c69c8d9aa4dd412b15b45359397442f82
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-09 22:07:23 +0900

    Change dev.exs (db settings)

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :nano_planner, NanoPlanner.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "phoenix",
+  password: "phoenix",
   database: "nano_planner_dev",
   hostname: "localhost",
   pool_size: 10
