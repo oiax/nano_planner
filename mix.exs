bc066468208ff083be2033279406f8236d919f3a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-09 16:02:30 +0900

    volume03 ch01(1)

--- mix.exs
+++ mix.exs
@@ -30,17 +30,17 @@ defmodule NanoPlanner.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.5"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:ecto, "~> 2.1.3"},
+     {:ecto, "~> 2.2.7"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:timex, "~> 3.1"},
-     {:timex_ecto, "~> 3.1"}]
+     {:timex_ecto, "~> 3.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
