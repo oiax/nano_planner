69682ad4b081a05f9be800cfdf202f1021233fdc
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-12-09 23:18:32 +0900

    Create Calendar, Calendar.PlanItem

---
+++ lib/nano_planner/calendar/plan_item.ex
@@ -0,0 +1,21 @@
+defmodule NanoPlanner.Calendar.PlanItem do
+  use Ecto.Schema
+  use Timex.Ecto.Timestamps
+  import Ecto.Changeset
+  alias NanoPlanner.Calendar.PlanItem
+
+  schema "plan_items" do
+    field :name, :string
+    field :description, :string
+    field :starts_at, Timex.Ecto.DateTime
+    field :ends_at, Timex.Ecto.DateTime
+
+    timestamps()
+  end
+
+  def changeset(%PlanItem{} = plan_item, attrs) do
+    plan_item
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
