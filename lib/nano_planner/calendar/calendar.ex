5dccc3c51ff9d8f65f017ae831842d146009c12a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-01-15 14:30:18 +0900

    Refactor Calendar.convert_datetime/1

--- lib/nano_planner/calendar/calendar.ex
+++ lib/nano_planner/calendar/calendar.ex
@@ -77,11 +77,9 @@ defmodule NanoPlanner.Calendar do
   defp convert_datetime(%PlanItem{} = item) do
     alias Timex.Timezone
 
-    time_zone = Application.get_env(:nano_planner, :default_time_zone)
-
     Map.merge(item, %{
-      starts_at: Timezone.convert(item.starts_at, time_zone),
-      ends_at: Timezone.convert(item.ends_at, time_zone)
+      starts_at: Timezone.convert(item.starts_at, time_zone()),
+      ends_at: Timezone.convert(item.ends_at, time_zone())
     })
   end
 end
