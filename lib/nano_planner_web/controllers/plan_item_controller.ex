a97b2d8da3e72d6cbb3f614fe5e1152c8b32b43a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-01-17 04:51:10 +0900

    ex03 ch08

--- lib/nano_planner_web/controllers/plan_item_controller.ex
+++ lib/nano_planner_web/controllers/plan_item_controller.ex
@@ -7,6 +7,16 @@ defmodule NanoPlannerWeb.PlanItemController do
     render conn, "index.html", plan_items: plan_items
   end
 
+  def new(conn, _params) do
+    plan_item = Calendar.build_plan_item
+    changeset = Calendar.change_plan_item(plan_item)
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, _params) do
+    redirect conn, to: plan_item_path(conn, :index)
+  end
+
   def show(conn, %{"id" => id}) do
     plan_item = Calendar.get_plan_item!(id)
     render conn, "show.html", plan_item: plan_item
