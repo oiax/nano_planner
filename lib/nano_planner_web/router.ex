400e6957bb7b4fd80b286973d566c7a1aade3b03
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-01-18 18:19:13 +0900

    ex03 ch13

--- lib/nano_planner_web/router.ex
+++ lib/nano_planner_web/router.ex
@@ -16,7 +16,6 @@ defmodule NanoPlannerWeb.Router do
     get "/lessons/form", LessonController, :form
     get "/lessons/register", LessonController, :register
     get "/lessons/hello", LessonController, :hello
-    resources "/plan_items", PlanItemController,
-      only: [:index, :new, :create, :show, :edit, :update]
+    resources "/plan_items", PlanItemController
   end
 end
