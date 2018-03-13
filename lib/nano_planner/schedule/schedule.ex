b67aa9e8980211227e23ad1ac5b4f56db615bc30
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-13 12:46:54 +0900

    Implement Schedule.list_continued_plan_items

--- lib/nano_planner/schedule/schedule.ex
+++ lib/nano_planner/schedule/schedule.ex
@@ -14,6 +14,16 @@ defmodule NanoPlanner.Schedule do
 
     PlanItem
     |> where([i], i.starts_at >= ^t0 and i.starts_at < ^t1)
+    |> or_where([i], i.ends_at > ^t0 and i.ends_at <= ^t1)
+    |> fetch_plan_items()
+  end
+
+  def list_continued_plan_items do
+    t0 = current_time() |> Timex.beginning_of_day()
+    t1 = t0 |> Timex.shift(hours: 24)
+
+    PlanItem
+    |> where([i], i.starts_at < ^t0 and i.starts_at > ^t1)
     |> fetch_plan_items()
   end
 
