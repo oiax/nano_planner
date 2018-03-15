8ee514c4bdde53834b1d32832424f5c04e9b7a64
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-15 23:09:16 +0900

    Experiment on Query API "like" function

--- priv/exp/filter0.exs
+++ priv/exp/filter0.exs
@@ -2,11 +2,11 @@ import Ecto.Query
 alias NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
 
-names = ~W(読書 買い物)
+pattern = "%買う%"
 
 items =
   PlanItem
-  |> where([i], i.name in ^names)
+  |> where([i], like(i.description, ^pattern))
   |> order_by(asc: :id)
   |> Repo.all()
   |> Enum.map(&(&1.name))
