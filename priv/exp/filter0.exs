fdc96d9a9ce43e1dfdb5cceee3abe337a83823e5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-15 22:09:17 +0900

    Add priv/exp/filter0.exs

---
+++ priv/exp/filter0.exs
@@ -0,0 +1,11 @@
+import Ecto.Query
+alias NanoPlanner.Repo
+alias NanoPlanner.Schedule.PlanItem
+
+items =
+  PlanItem
+  |> order_by(asc: :id)
+  |> Repo.all()
+  |> Enum.map(&(&1.name))
+
+IO.inspect(items)
