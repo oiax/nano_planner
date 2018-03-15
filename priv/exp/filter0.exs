deb4cc973a8a9c970d5d2d6b9c4e339f2b60237d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-15 22:44:20 +0900

    Experiment on query interpolation

--- priv/exp/filter0.exs
+++ priv/exp/filter0.exs
@@ -2,8 +2,11 @@ import Ecto.Query
 alias NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
 
+name = "読書"
+
 items =
   PlanItem
+  |> where(name: ^name)
   |> order_by(asc: :id)
   |> Repo.all()
   |> Enum.map(&(&1.name))
