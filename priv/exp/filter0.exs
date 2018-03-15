a6810a9e99ce8164b86e3458f92d2f8e9c57f7eb
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-15 23:00:44 +0900

    Experiment on "in" operator

--- priv/exp/filter0.exs
+++ priv/exp/filter0.exs
@@ -2,11 +2,11 @@ import Ecto.Query
 alias NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
 
-name = "読書"
+names = ~W(読書 買い物)
 
 items =
   PlanItem
-  |> where([i], i.name == ^name)
+  |> where([i], i.name in ^names)
   |> order_by(asc: :id)
   |> Repo.all()
   |> Enum.map(&(&1.name))
