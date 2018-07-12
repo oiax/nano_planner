b47d30d88b87bce872563112522fc66a7fa66835
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-13 02:28:21 +0900

    Add Counter.Agent

---
+++ lib/counter/agent.ex
@@ -0,0 +1,24 @@
+defmodule Counter.Agent do
+  use Agent
+
+  @doc """
+  Starts a new counter.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> 0 end)
+  end
+
+  @doc """
+  Gets the current value.
+  """
+  def get_value(agent) do
+    Agent.get(agent, fn value -> value end)
+  end
+
+  @doc """
+  Increment the current value by one.
+  """
+  def increment(agent) do
+    Agent.update(agent, fn value -> value + 1 end)
+  end
+end
