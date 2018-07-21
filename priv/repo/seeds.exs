c2070a4943a7a7ed2c6ac97eda1a9da9b96bf9a7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-22 00:05:21 +0900

    Add all_day, starts_on, ends_on to plan_items table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -38,3 +38,25 @@ insert! %PlanItem{
   starts_at: Timex.shift(time0, days: -1, hours: 10),
   ends_at: Timex.shift(time0, days: 1, hours: 17)
 }
+
+date0 = Timex.to_date(time0)
+
+insert! %PlanItem{
+  name: "私の誕生日",
+  description: "",
+  all_day: true,
+  starts_at: Timex.shift(time0, days: 7),
+  ends_at: Timex.shift(time0, days: 8),
+  starts_on: Date.add(date0, 7),
+  ends_on: Date.add(date0, 7)
+}
+
+insert! %PlanItem{
+  name: "社員研修",
+  description: "都内某所",
+  all_day: true,
+  starts_at: Timex.shift(time0, days: 14),
+  ends_at: Timex.shift(time0, days: 17),
+  starts_on: Date.add(date0, 14),
+  ends_on: Date.add(date0, 16)
+}
