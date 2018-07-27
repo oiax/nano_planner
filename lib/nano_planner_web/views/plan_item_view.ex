a32aca9851eef7caa614858f5919a15d481a80b1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-28 02:38:11 +0900

    Date duration, format date

--- lib/nano_planner_web/views/plan_item_view.ex
+++ lib/nano_planner_web/views/plan_item_view.ex
@@ -85,6 +85,11 @@ defmodule NanoPlannerWeb.PlanItemView do
     Strftime.format!(datetime, "%Y年%-m月%-d日 (#{w}) %H:%M")
   end
 
+  def format_date(date) do
+    w = format_wday(date)
+    Strftime.format!(date, "%Y年%-m月%-d日 (#{w})")
+  end
+
   @weekday_names ~w(日 月 火 水 木 金 土)
   defp format_wday(datetime) do
     Enum.at(@weekday_names, Timex.days_to_beginning_of_week(datetime, :sun))
