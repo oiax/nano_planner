80a55f4225cd28519624fe6a4afcff84ed35cef0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-15 16:59:44 +0900

    JS: toggle fields

--- assets/js/plan_item_form.js
+++ assets/js/plan_item_form.js
@@ -1,10 +1,19 @@
 import $ from "jquery";
 
 $(() => {
-  const checkBox = $("#plan_item_all_day");
+  const form = $("form.plan-item")
 
-  if (checkBox.length > 0) {
-    const allDay = checkBox.prop("checked");
-    console.log(allDay);
+  if (form.length > 0) {
+    const checkBox = $("#plan_item_all_day");
+
+    const toggleInputFields = () => {
+      const allDay = checkBox.prop("checked");
+      form.find(".js-date-and-time").toggle(!allDay);
+      form.find(".js-date-picker").toggle(allDay);
+    }
+
+    toggleInputFields();
+
+    checkBox.on("change", toggleInputFields)
   }
 });
