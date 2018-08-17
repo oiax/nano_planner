3d2b7ae99d8bb6b670a45bcfdf0334f4fd928ac0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-17 22:08:19 +0900

    Format priv/exp/validations.exs

--- priv/exp/validation.exs
+++ priv/exp/validation.exs
@@ -2,7 +2,7 @@ import Ecto.Query
 alias NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
 
-item = PlanItem |> first |> Repo.one
+item = PlanItem |> first |> Repo.one()
 attrs = %{name: "TEST"}
 cs = PlanItem.changeset(item, attrs)
 
