77c099c5ac4c7c0d4ca433e12b9f56e398a3fad0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-18 13:58:26 +0900

    bootstrap_feedback/3

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -1,5 +1,7 @@
 defmodule NanoPlannerWeb.BootstrapHelpers do
+  use Phoenix.HTML
   import NanoPlannerWeb.HtmlHelpers, only: [add_class: 2, add_class: 3]
+  import NanoPlannerWeb.ErrorHelpers, only: [translate_error: 1]
 
   def bootstrap_text_input(form, field, opts \\ []) do
     class_attr =
@@ -9,5 +11,17 @@ defmodule NanoPlannerWeb.BootstrapHelpers do
 
     opts = Keyword.put(opts, :class, class_attr)
     Phoenix.HTML.Form.text_input(form, field, opts)
+  end
+
+  def bootstrap_feedback(form, field, opts \\ []) do
+    class_attr =
+      Keyword.get(opts, :class, "")
+      |> add_class("invalid-feedback", Keyword.has_key?(form.errors, field))
+
+    opts = Keyword.put(opts, :class, class_attr)
+
+    Enum.map(Keyword.get_values(form.errors, field), fn error ->
+      content_tag(:div, translate_error(error), opts)
+    end)
   end
 end
