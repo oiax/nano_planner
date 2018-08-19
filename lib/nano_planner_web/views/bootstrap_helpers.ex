0d686e0da42b0d3b5a75871c122e1f700918adea
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-19 22:20:13 +0900

    add_form_control_classes

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -4,21 +4,19 @@ defmodule NanoPlannerWeb.BootstrapHelpers do
   import NanoPlannerWeb.ErrorHelpers, only: [translate_error: 1]
 
   def bootstrap_text_input(form, field, opts \\ []) do
-    opts =
-      opts
-      |> add_class("form-control")
-      |> add_class("is-invalid", Keyword.has_key?(form.errors, field))
-
+    opts = add_form_control_classes(form, field, opts)
     Phoenix.HTML.Form.text_input(form, field, opts)
   end
 
   def bootstrap_textarea(form, field, opts \\ []) do
-    opts =
-      opts
-      |> add_class("form-control")
-      |> add_class("is-invalid", Keyword.has_key?(form.errors, field))
-
+    opts = add_form_control_classes(form, field, opts)
     Phoenix.HTML.Form.textarea(form, field, opts)
+  end
+
+  defp add_form_control_classes(form, field, opts) do
+    opts
+    |> add_class("form-control")
+    |> add_class("is-invalid", Keyword.has_key?(form.errors, field))
   end
 
   def bootstrap_feedback(form, field, opts \\ []) do
