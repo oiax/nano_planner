727d605a475d860d6e33fa425d7794a61e9fe7d6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-19 19:14:27 +0900

    feedback_full_message

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -18,25 +18,28 @@ defmodule NanoPlannerWeb.BootstrapHelpers do
       |> add_class("invalid-feedback", Keyword.has_key?(form.errors, field))
 
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      subject =
-        Gettext.dgettext(
-          NanoPlannerWeb.Gettext,
-          "schema",
-          "plan_item|#{field}"
-        )
-
-      predicate = translate_error(error)
-
-      message =
-        Gettext.dgettext(
-          NanoPlannerWeb.Gettext,
-          "errors",
-          "%{subject} %{predicate}.",
-          subject: subject,
-          predicate: predicate
-        )
-
-      content_tag(:div, message, opts)
+      attribute = feedback_attribute(form, field)
+      message = translate_error(error)
+      full_message = feedback_full_message(attribute, message)
+      content_tag(:div, full_message, opts)
     end)
+  end
+
+  defp feedback_attribute(form, field) do
+    Gettext.dgettext(
+      NanoPlannerWeb.Gettext,
+      "schema",
+      "#{form.name}|#{field}"
+    )
+  end
+
+  defp feedback_full_message(attribute, message) do
+    Gettext.dgettext(
+      NanoPlannerWeb.Gettext,
+      "errors",
+      "%{attribute} %{message}.",
+      attribute: attribute,
+      message: message
+    )
   end
 end
