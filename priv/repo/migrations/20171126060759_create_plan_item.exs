a10f39e8229e247a2b8e63d4a2428efc706b7407
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-02 23:25:49 +0900

    Run mix.format

--- priv/repo/migrations/20171126060759_create_plan_item.exs
+++ priv/repo/migrations/20171126060759_create_plan_item.exs
@@ -3,13 +3,12 @@ defmodule NanoPlanner.Repo.Migrations.CreatePlanItem do
 
   def change do
     create table(:plan_items) do
-      add :name, :string, null: false
-      add :description, :text, null: false
-      add :starts_at, :utc_datetime, null: false
-      add :ends_at, :utc_datetime, null: false
+      add(:name, :string, null: false)
+      add(:description, :text, null: false)
+      add(:starts_at, :utc_datetime, null: false)
+      add(:ends_at, :utc_datetime, null: false)
 
       timestamps()
     end
-
   end
 end
