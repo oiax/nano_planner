6d7b5bf7f255fcd83e0032e9b827a6da8f8eefc8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-04 22:45:26 +0900

    ch05: filter.exs (1)

--- priv/exp/filter.exs
+++ priv/exp/filter.exs
@@ -2,8 +2,17 @@ import Ecto.Query
 alias NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
 
+name = "読書"
+time0 = Timex.now("Asia/Tokyo") |> Timex.beginning_of_day()
+time1 = time0 |> Timex.shift(days: 1)
+time2 = time0 |> Timex.shift(days: 7)
+
 items =
   PlanItem
+  |> where(
+    [i],
+    i.name != ^name or (i.starts_at > ^time1 and i.starts_at < ^time2)
+  )
   |> order_by(asc: :id)
   |> Repo.all()
   |> Enum.map(& &1.name)
