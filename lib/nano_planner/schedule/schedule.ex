4b1d0282b2584345805e536b956f9f235c703acd
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-05 22:32:20 +0900

    ch07 (1)

--- lib/nano_planner/schedule/schedule.ex
+++ lib/nano_planner/schedule/schedule.ex
@@ -13,13 +13,17 @@ defmodule NanoPlanner.Schedule do
     t1 = t0 |> Timex.shift(hours: 24)
 
     PlanItem
-    |> where([i], i.starts_at >= ^t0 and i.starts_at < ^t1)
+    |> where(
+      [i],
+      (i.starts_at >= ^t0 and i.starts_at < ^t1) or
+        (i.ends_at > ^t0 and i.ends_at <= ^t1)
+    )
     |> fetch_plan_items()
   end
 
   defp fetch_plan_items(query) do
     query
-    |> order_by(asc: :starts_at, asc: :all_day, asc: :ends_at, asc: :id)
+    |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
     |> Repo.all()
     |> convert_datetime()
   end
