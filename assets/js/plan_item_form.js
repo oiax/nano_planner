4901cea40e7f75cc974a076f29464e37e73d8c95
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-10 23:42:55 +0900

    ch12 (3)

--- assets/js/plan_item_form.js
+++ assets/js/plan_item_form.js
@@ -3,5 +3,11 @@ import $ from "jquery";
 $(() => {
   const form = $("form.js-plan-item")
 
-  if (form.length > 0) console.log("Loaded!");
+  if (form.length > 0) {
+    const checkBox =
+      form.find("input[name='plan_item[all_day]'][type='checkbox']");
+
+    const allDay = checkBox.prop("checked");
+    console.log(allDay);
+  }
 });
