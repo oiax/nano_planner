ff89c91534240a460659ea8ea2b3ef4b59143bd1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-12 00:10:07 +0900

    ch12 (7)

--- assets/js/plan_item_form.js
+++ assets/js/plan_item_form.js
@@ -9,9 +9,18 @@ $(() => {
 
     const toggleInputFields = () => {
       const allDay = checkBox.prop("checked");
-      form.find(".js-date-and-time").toggle(!allDay);
-      form.find(".js-date-picker").toggle(allDay);
-    })
+      const div1 = form.find(".js-date-and-time");
+      const div2 = form.find(".js-date-picker");
+
+      if (allDay) {
+        div1.hide().find("input").removeAttr("required");
+        div2.show().find("input").attr("required", true);
+      }
+      else {
+        div1.show().find("input").attr("required", true);
+        div2.hide().find("input").removeAttr("required");
+      }
+    };
 
     toggleInputFields();
 
