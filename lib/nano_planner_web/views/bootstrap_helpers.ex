59954a42db9177db762bc5f094ea64d5b9620b83
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-24 01:02:48 +0900

    Make simpler BootstrapHelpers

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -1,58 +1,57 @@
 defmodule NanoPlannerWeb.BootstrapHelpers do
   use Phoenix.HTML
-  import NanoPlannerWeb.HtmlHelpers, only: [add_class: 2, add_class: 3]
   import NanoPlannerWeb.ErrorHelpers, only: [translate_error: 1]
 
   def bootstrap_text_input(form, field, opts \\ []) do
-    opts = add_form_control_classes(form, field, opts)
+    opts = adjust_opts(form, field, opts)
     Phoenix.HTML.Form.text_input(form, field, opts)
   end
 
   def bootstrap_date_input(form, field, opts \\ []) do
-    opts = add_form_control_classes(form, field, opts)
+    opts = adjust_opts(form, field, opts)
     Phoenix.HTML.Form.date_input(form, field, opts)
   end
 
   def bootstrap_textarea(form, field, opts \\ []) do
-    opts = add_form_control_classes(form, field, opts)
+    opts = adjust_opts(form, field, opts)
     Phoenix.HTML.Form.textarea(form, field, opts)
   end
 
   def bootstrap_select(form, field, options, opts \\ []) do
-    opts = add_form_control_classes(form, field, opts)
+    opts = adjust_opts(form, field, opts)
     Phoenix.HTML.Form.select(form, field, options, opts)
   end
 
-  defp add_form_control_classes(form, field, opts) do
-    invalid =
+  defp adjust_opts(form, field, opts) do
+    class = form_control_class(form, field, opts)
+
+    opts
+    |> Keyword.put(:class, class)
+    |> Keyword.delete(:parent)
+  end
+
+  defp form_control_class(form, field, opts) do
+    has_error? =
       Keyword.has_key?(form.errors, field) ||
         Keyword.has_key?(form.errors, opts[:parent])
 
     opts
-    |> Keyword.delete(:parent)
+    |> Keyword.get(:class, "")
     |> add_class("form-control")
-    |> add_class("is-invalid", invalid)
+    |> add_class("is-invalid", has_error?)
   end
 
-  def boostrap_custom_checkbox(form, field, label_text) do
-    content_tag(:div, class: "custom-control custom-checkbox") do
-      [
-        checkbox(form, field, class: "custom-control-input"),
-        label(form, field, label_text, class: "custom-control-label")
-      ]
-    end
-  end
+  defp add_class("" = _class, new_class), do: new_class
+  defp add_class(class, new_class), do: class <> " " <> new_class
+  defp add_class(class, new_class, true), do: add_class(class, new_class)
+  defp add_class(class, _new_class, false), do: class
 
-  def bootstrap_feedback(form, field, opts \\ []) do
-    opts =
-      opts
-      |> add_class("invalid-feedback", Keyword.has_key?(form.errors, field))
-
+  def bootstrap_feedback(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       attribute = feedback_attribute(form, field)
       message = translate_error(error)
       full_message = feedback_full_message(attribute, message)
-      content_tag(:div, full_message, opts)
+      content_tag(:div, full_message, class: "invalid-feedback")
     end)
   end
 
@@ -72,5 +71,14 @@ defmodule NanoPlannerWeb.BootstrapHelpers do
       attribute: attribute,
       message: message
     )
+  end
+
+  def boostrap_custom_checkbox(form, field, label_text) do
+    content_tag(:div, class: "custom-control custom-checkbox") do
+      [
+        checkbox(form, field, class: "custom-control-input"),
+        label(form, field, label_text, class: "custom-control-label")
+      ]
+    end
   end
 end
