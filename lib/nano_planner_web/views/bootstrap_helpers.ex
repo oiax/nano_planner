a7ff719154aabf2efe54e5fa162d7f0746376fa3
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-24 00:39:47 +0900

    ch13 (7)

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -16,21 +16,23 @@ defmodule NanoPlannerWeb.BootstrapHelpers do
     |> add_class("is-invalid", Keyword.has_key?(form.errors, field))
   end
 
-  defp add_class(class, new_class) do
-    if class == "" do
-      new_class
-    else
-      class <> " " <> new_class
-    end
-  end
-
+  defp add_class("" = _class, new_class), do: new_class
+  defp add_class(class, new_class), do: class <> " " <> new_class
   defp add_class(class, new_class, true), do: add_class(class, new_class)
-
   defp add_class(class, _new_class, false), do: class
 
   def bootstrap_feedback(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:div, translate_error(error), class: "invalid-feedback")
     end)
+  end
+
+  def boostrap_custom_checkbox(form, field, label_text) do
+    content_tag(:div, class: "custom-control custom-checkbox") do
+      [
+        checkbox(form, field, class: "custom-control-input"),
+        label(form, field, label_text, class: "custom-control-label")
+      ]
+    end
   end
 end
