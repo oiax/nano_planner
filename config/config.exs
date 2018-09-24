d072ad4123a6a5437473b11a009b5731721855ee
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-25 00:01:54 +0900

    ch14 (1)

--- config/config.exs
+++ config/config.exs
@@ -22,8 +22,15 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Use Jason for JSON parsing in Phoenix and Ecto
+config :phoenix, :json_library, Jason
+config :ecto, :json_library, Jason
+
 # Configures the default time zone
 config :nano_planner, default_time_zone: "Asia/Tokyo"
+
+# Configures the default locale
+config :nano_planner, NanoPlannerWeb.Gettext, default_locale: "ja"
 
 # Use Jason for JSON parsing in Phoenix and Ecto
 config :phoenix, :json_library, Jason
