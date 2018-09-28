e9c8755555ab26f23d9ec6dab5be5636a25b461e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-28 21:38:42 +0900

    ch14 (4)

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -1,5 +1,14 @@
 defmodule NanoPlannerWeb.BootstrapHelpers do
-  def form_control_class(form, field) do
+  import Phoenix.HTML.Form
+
+  def bootstrap_text_input(form, field, opts \\ []) do
+    class = form_control_class(form, field)
+    opts = Keyword.put(opts, :class, class)
+
+    text_input(form, field, opts)
+  end
+
+  defp form_control_class(form, field) do
     if Keyword.has_key?(form.errors, field) do
       "form-control is-invalid"
     else
