d8818e070538c193a84aaef7aac1d1ccd9500b06
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-09-30 00:51:21 +0900

    ch16 (5) fin.

--- lib/nano_planner/schedule/plan_item.ex
+++ lib/nano_planner/schedule/plan_item.ex
@@ -109,5 +109,6 @@ defmodule NanoPlanner.Schedule.PlanItem do
     changeset
     |> validate_required([:name])
     |> validate_length(:name, max: 80)
+    |> validate_length(:description, max: 400)
   end
 end
