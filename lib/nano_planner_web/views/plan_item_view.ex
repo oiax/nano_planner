450eee428a347faeb0f731fb8e36c9eb430d7000
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-04-27 23:26:46 +0900

    ch16: finished

--- lib/nano_planner_web/views/plan_item_view.ex
+++ lib/nano_planner_web/views/plan_item_view.ex
@@ -8,15 +8,18 @@ defmodule NanoPlannerWeb.PlanItemView do
 
   defp format_starts_at(item) do
     time_zone = Application.get_env(:nano_planner, :default_time_zone)
+    w = format_wday(item.starts_at)
 
     if item.starts_at.year == DateTime.now!(time_zone).year do
-      Strftime.format!(item.starts_at, "%-m月%-d日 %H:%M")
+      Strftime.format!(item.starts_at, "%-m月%-d日 (#{w}) %H:%M")
     else
-      Strftime.format!(item.starts_at, "%Y年%-m月%-d日 %H:%M")
+      Strftime.format!(item.starts_at, "%Y年%-m月%-d日 (#{w}) %H:%M")
     end
   end
 
   defp format_ends_at(item) do
+    w = format_wday(item.ends_at)
+
     cond do
       DateTime.to_date(item.ends_at) == DateTime.to_date(item.starts_at) ->
         Strftime.format!(item.ends_at, "%H:%M")
@@ -25,7 +28,18 @@ defmodule NanoPlannerWeb.PlanItemView do
         Strftime.format!(item.ends_at, "%-m月%-d日 %H:%M")
 
       true ->
-        Strftime.format!(item.ends_at, "%Y年%-m月%-d日 %H:%M")
+        Strftime.format!(item.ends_at, "%Y年%-m月%-d日 (#{w}) %H:%M")
     end
+  end
+
+  defp format_datetime(datetime) do
+    w = format_wday(datetime)
+    Strftime.format!(datetime, "%Y年%-m月%-d日 (#{w}) %H:%M")
+  end
+
+  @weekday_names ~w(日 月 火 水 木 金 土)
+  defp format_wday(datetime) do
+    wday = Timex.days_to_beginning_of_week(datetime, :sun)
+    Enum.at(@weekday_names, wday)
   end
 end
