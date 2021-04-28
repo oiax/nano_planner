4e83ac1928ec90e65ecd0bc0d78120de3499e4f1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-04-28 22:01:17 +0900

    ex03/ch03

---
+++ lib/nano_planner_web/controllers/lesson_controller.ex
@@ -0,0 +1,11 @@
+defmodule NanoPlannerWeb.LessonController do
+  use NanoPlannerWeb, :controller
+
+  def form(conn, _params) do
+    render(conn, "form.html")
+  end
+
+  def register(conn, %{"user" => user_params}) do
+    render(conn, "register.html", user_name: user_params["name"])
+  end
+end
--- lib/nano_planner_web/router.ex
+++ lib/nano_planner_web/router.ex
@@ -13,6 +13,8 @@ defmodule NanoPlannerWeb.Router do
     pipe_through :browser
 
     get "/", TopController, :index
+    get "/lessons/form", LessonController, :form
+    get "/lessons/register", LessonController, :register
     get "/plan_items", PlanItemController, :index
     get "/plan_items/:id", PlanItemController, :show
   end
