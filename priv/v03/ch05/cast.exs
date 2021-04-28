571043a2862aba6ba99815eef4e594543de47907
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-04-28 23:25:28 +0900

    ex03/ch05

---
+++ priv/v03/ch05/cast.exs
@@ -0,0 +1,18 @@
+alias NanoPlanner.Schedule.PlanItem
+
+item = %PlanItem{}
+
+params = %{
+  "name" => "Test",
+  "description" => "Experiment",
+  "starts_at" => "2020-04-01T12:00:00",
+  "ends_at" => "2020-04-01T13:00:00"
+}
+
+fields = [:name, :description, :starts_at, :ends_at]
+cs = Ecto.Changeset.cast(item, params, fields)
+
+IO.inspect(Map.keys(cs))
+IO.inspect(cs.data == item)
+IO.inspect(cs.params == params)
+IO.inspect(cs.changes)
