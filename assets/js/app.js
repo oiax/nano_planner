15a5741cf088bec79484d730ef4d17d1b76b76cc
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-05-02 17:59:02 +0900

    ex03/ch12: modal_viewer.js を verndor から js/app に移動

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,5 @@ import "@fortawesome/fontawesome-free/js/fontawesome"
 import "@fortawesome/fontawesome-free/js/solid"
 import "@fortawesome/fontawesome-free/js/regular"
 import "@fortawesome/fontawesome-free/js/brands"
+
+import "./app/modal_viewer"
