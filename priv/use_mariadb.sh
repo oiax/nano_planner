2fe2ffcbfca8a71aecfdcfb0162b2db6e82be20a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-05-04 14:53:15 +0900

    Create priv/use_{postgresq,mariadb}.sh

---
+++ priv/use_mariadb.sh
@@ -0,0 +1,5 @@
+sed -i -e 's/:postgrex/:myxql/' mix.exs
+sed -i -e 's/Postgres/MyXQL/' lib/nano_planner/repo.ex
+sed -i -e 's/username: "postgres"/username: "root"/' config/dev.exs
+sed -i -e 's/password: "postgres"/password: "root"/' config/dev.exs
+sed -i -e 's/hostname: "postgres"/hostname: "mariadb"/' config/dev.exs
