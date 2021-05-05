057cf3d008952f399f91d963e81cde7121092bcf
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-05-05 20:11:53 +0900

    [ex02] Fix priv/use_{postgresql,mariadb}.sh

--- priv/use_postgresql.sh
+++ priv/use_postgresql.sh
@@ -3,3 +3,4 @@ sed -i -e 's/MyXQL/Postgres/' lib/nano_planner/repo.ex
 sed -i -e 's/username: "root"/username: "postgres"/' config/dev.exs
 sed -i -e 's/password: "root"/password: "postgres"/' config/dev.exs
 sed -i -e 's/hostname: "mariadb"/hostname: "postgres"/' config/dev.exs
+mix deps.get
