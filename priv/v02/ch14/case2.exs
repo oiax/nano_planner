76932c392d5c0ffb223e567b1ec98a1389661119
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-05-06 09:55:19 +0900

    [ex02] ch14: case2.exs

--- priv/v02/ch14/case2.exs
+++ priv/v02/ch14/case2.exs
@@ -1,10 +1,17 @@
-list = [100, [1, 2, 3], %{value: 7}, true]
+list = [
+  100,
+  [1, 2, 3],
+  %User{name: "Alice"},
+  %{value: 7},
+  true
+]
 
 for e <- list do
   x =
     case e do
       n when is_integer(n) -> n
       l when is_list(l) -> Enum.join(l, ":")
+      %User{} = u -> "name = #{u.name}"
       m when is_map(m) -> "value = #{m.value}"
       b when is_boolean(b) -> b
     end
