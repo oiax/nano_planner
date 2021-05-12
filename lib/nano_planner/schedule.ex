7552d72d6f805018b54e73ba331dcfab78e98db5
makoto-atarashi <46238763+makoto-atarashi@users.noreply.github.com>
2021-05-12 12:17:21 +0900

    [ex03] Fix schedule.ex

--- lib/nano_planner/schedule.ex
+++ lib/nano_planner/schedule.ex
@@ -127,11 +127,9 @@ defmodule NanoPlanner.Schedule do
   end
 
   defp convert_datetime(%PlanItem{} = item) do
-    time_zone = Application.get_env(:nano_planner, :default_time_zone)
-
     Map.merge(item, %{
-      starts_at: DateTime.shift_zone!(item.starts_at, time_zone),
-      ends_at: DateTime.shift_zone!(item.ends_at, time_zone)
+      starts_at: DateTime.shift_zone!(item.starts_at, time_zone()),
+      ends_at: DateTime.shift_zone!(item.ends_at, time_zone())
     })
   end
 
