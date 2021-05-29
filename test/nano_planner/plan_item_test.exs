fd0c33e0a16154b344ad833e0591d78d2292fd3a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-05-29 11:53:09 +0900

    put_change/3 vs. cast/3

--- test/nano_planner/plan_item_test.exs
+++ test/nano_planner/plan_item_test.exs
@@ -23,6 +23,33 @@ defmodule NanoPlanner.Schedule.PlanItemTest do
 
       f = Phoenix.HTML.Form.form_for(cs, "#")
       assert Phoenix.HTML.Form.input_value(f, :all_day) == true
+
+      # Changeset 構造体の changes フィールドを直接書き換える。
+      cs = Ecto.Changeset.put_change(cs, :all_day, false)
+      assert Ecto.Changeset.get_field(cs, :all_day) == false
+
+      # PlanItem 構造体の all_day フィールドのデフォルト値は false のため、changes フィールドから除外される。
+      refute Map.has_key?(cs.changes, :all_day)
+
+      # しかし、input_value/2 は "true" を返す。
+      # なぜなら、Changeset 構造体の params フィールドにフォームから入力された値が残っているから。
+      #
+      # Phoenix.HTML.Form.input_value/2 の公式ドキュメントによれば、次の順で値を取りに行く。
+      #   1. changes
+      #   2. params
+      #   3. data
+      f = Phoenix.HTML.Form.form_for(cs, "#")
+
+      assert cs.params["all_day"] == "true"
+      assert Phoenix.HTML.Form.input_value(f, :all_day) == "true"
+
+      # 正しくは、cast/3 を用いる。こうすれば params フィールドと changes フィールドが適切に更新される。
+      cs = Ecto.Changeset.cast(cs, %{"all_day" => "false"}, [:all_day])
+
+      f = Phoenix.HTML.Form.form_for(cs, "#")
+
+      assert cs.params["all_day"] == "false"
+      assert Phoenix.HTML.Form.input_value(f, :all_day) == "false"
     end
 
     test "バリデーション失敗の場合" do
