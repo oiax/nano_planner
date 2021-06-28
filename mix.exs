4c77f91f726b1bfd284fbeaf9f120c1087b21510
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-06-28 09:56:37 +0900

    Use Phoenix 1.6-dev

--- mix.exs
+++ mix.exs
@@ -33,9 +33,12 @@ defmodule NanoPlanner.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
+      {:phoenix,
+       git: "https://github.com/phoenixframework/phoenix.git",
+       rev: "37ccc1f4ded9d0f8c1acb4e24d41d9c7ac254f27",
+       override: true},
       {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
