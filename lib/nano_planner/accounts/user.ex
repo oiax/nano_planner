1701f3d524872379adc072729c1d0863e632de30
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-06-29 01:58:18 +0900

    seed users table

--- lib/nano_planner/accounts/user.ex
+++ lib/nano_planner/accounts/user.ex
@@ -1,19 +1,11 @@
 defmodule NanoPlanner.Accounts.User do
   use Ecto.Schema
-  import Ecto.Changeset
+  import Ecto.Changeset, warn: false
 
   schema "users" do
     field :login_name, :string
-    field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
 
-    timestamps()
-  end
-
-  @doc false
-  def changeset(user, attrs) do
-    user
-    |> cast(attrs, [])
-    |> validate_required([])
+    timestamps(type: :utc_datetime_usec)
   end
 end
