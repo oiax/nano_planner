ed721cece193ac2cfd9f91a99b0f9435fc029c15
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-06-29 11:14:52 +0900

    create 5 users for accounts_test

--- test/nano_planner/accounts_test.exs
+++ test/nano_planner/accounts_test.exs
@@ -8,6 +8,18 @@ defmodule NanoPlanner.AccountsTest do
     end
 
     test "登録されているユーザーの数を返す" do
+      import NanoPlanner.Repo, only: [insert!: 1]
+      alias NanoPlanner.Accounts.User
+
+      for login_name <- ~w(alice bob carol david eve) do
+        hashed_password = Bcrypt.hash_pwd_salt(login_name <> "123!")
+
+        insert!(%User{
+          login_name: login_name,
+          hashed_password: hashed_password
+        })
+      end
+
       assert Accounts.count_users() == 5
     end
   end
