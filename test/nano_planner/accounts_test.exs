8b209b9270365660c25174efc247db7459993694
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-06-29 14:12:36 +0900

    Add tests for get_user_by_login_name_and_password/2 (failing)

--- test/nano_planner/accounts_test.exs
+++ test/nano_planner/accounts_test.exs
@@ -16,4 +16,38 @@ defmodule NanoPlanner.AccountsTest do
       assert Accounts.count_users() == 5
     end
   end
+
+  describe "get_user_by_login_name_and_password/2" do
+    test "ログイン名が存在しない場合はユーザーを返さない" do
+      fetched =
+        Accounts.get_user_by_login_name_and_password(
+          "unknown",
+          "unknown123!"
+        )
+
+      assert fetched == nil
+    end
+
+    test "パスワードが正しくなければユーザーを返さない" do
+      user = user_fixture(login_name: "alice")
+
+      fetched =
+        Accounts.get_user_by_login_name_and_password(
+          user.login_name,
+          "invalid"
+        )
+
+      assert fetched == nil
+    end
+
+    test "ログイン名とパスワードが正しければユーザーを返す" do
+      user = user_fixture(login_name: "alice")
+
+      fetched =
+        Accounts.get_user_by_login_name_and_password("alice", "alice123!")
+
+      assert %Accounts.User{} = fetched
+      assert fetched.id == user.id
+    end
+  end
 end
