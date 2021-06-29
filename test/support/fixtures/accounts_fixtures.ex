9a3be91dac93b33336c7127a03c96578f15e589a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-06-29 11:34:51 +0900

    AccountsFixtures

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,13 @@
+defmodule NanoPlanner.AccountsFixtures do
+  import NanoPlanner.Repo, only: [insert!: 1]
+  alias NanoPlanner.Accounts.User
+
+  def user_fixture(attrs) do
+    hashed_password = Bcrypt.hash_pwd_salt(attrs[:login_name] <> "123!")
+
+    insert!(%User{
+      login_name: attrs[:login_name],
+      hashed_password: hashed_password
+    })
+  end
+end
