f35251381019f6b1e777dafa8f4c9a4d6d107e9f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-11 01:24:07 +0900

    UserSessionController.create/2

--- lib/nano_planner_web/controllers/user_session_controller.ex
+++ lib/nano_planner_web/controllers/user_session_controller.ex
@@ -1,7 +1,23 @@
 defmodule NanoPlannerWeb.UserSessionController do
   use NanoPlannerWeb, :controller
+  alias NanoPlannerWeb.UserAuth
+  alias NanoPlanner.Accounts
 
   def new(conn, _params) do
     render(conn, "new.html")
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    user =
+      Accounts.get_user_by_login_name_and_password(
+        user_params["login_name"],
+        user_params["password"]
+      )
+
+    if user do
+      UserAuth.log_in_user(conn, user, user_params)
+    else
+      render(conn, "new.html")
+    end
   end
 end
