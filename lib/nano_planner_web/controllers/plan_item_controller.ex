4a8bed6c207d5d1c6ab72211e56f309c8b1b1d04
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-11 23:20:29 +0900

    Finish ex05

--- lib/nano_planner_web/controllers/plan_item_controller.ex
+++ lib/nano_planner_web/controllers/plan_item_controller.ex
@@ -28,7 +28,9 @@ defmodule NanoPlannerWeb.PlanItemController do
   end
 
   def create(conn, %{"plan_item" => plan_item_params}) do
-    case Schedule.create_plan_item(plan_item_params) do
+    owner = conn.assigns[:current_user]
+
+    case Schedule.create_plan_item(plan_item_params, owner) do
       {:ok, _plan_item} ->
         conn
         |> put_flash(:info, "予定を追加しました。")
