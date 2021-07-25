c72b129758fe48f4ad184270474eba190b1ac09d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-25 15:00:55 +0900

    [ex05/ch04] get_user_by_login_name_and_password/2

--- lib/nano_planner/accounts.ex
+++ lib/nano_planner/accounts.ex
@@ -5,4 +5,15 @@ defmodule NanoPlanner.Accounts do
   def count_users do
     Repo.aggregate(User, :count, :id)
   end
+
+  def get_user_by_login_name_and_password(login_name, password) do
+    user = Repo.get_by(User, login_name: login_name)
+
+    if user do
+      if Bcrypt.verify_pass(password, user.hashed_password), do: user
+    else
+      Bcrypt.no_user_verify()
+      nil
+    end
+  end
 end
