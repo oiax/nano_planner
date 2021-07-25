af68d5e48f86c33b2d55bbf1468e2766c4eaf30d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-25 14:40:06 +0900

    [ex05/ch03] Create accounts_user_fixtures.ex

--- test/nano_planner/accounts_test.exs
+++ test/nano_planner/accounts_test.exs
@@ -1,6 +1,7 @@
 defmodule NanoPlanner.AccountsTest do
   use NanoPlanner.DataCase
   alias NanoPlanner.Accounts
+  import NanoPlanner.AccountsFixtures
 
   describe "count_users/0" do
     test "初期状態では0を返す" do
@@ -8,16 +9,8 @@ defmodule NanoPlanner.AccountsTest do
     end
 
     test "登録されているユーザーの数を返す" do
-      import NanoPlanner.Repo, only: [insert!: 1]
-      alias NanoPlanner.Accounts.User
-
       for login_name <- ~w(alice bob carol david eve) do
-        hashed_password = Bcrypt.hash_pwd_salt(login_name <> "123!")
-
-        insert!(%User{
-          login_name: login_name,
-          hashed_password: hashed_password
-        })
+        user_fixture(login_name: login_name)
       end
 
       assert Accounts.count_users() == 5
