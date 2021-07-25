20985cc3e12ba24637b8bf4fbf59e0e45d6c93eb
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-25 13:35:04 +0900

    [ex05/ch02] seeds for users

---
+++ priv/repo/seeds/users.exs
@@ -0,0 +1,11 @@
+import NanoPlanner.Repo, only: [insert!: 1] 
+alias NanoPlanner.Accounts.User
+
+for login_name <- ~w(alice bob carol david eve) do
+  hashed_password = Bcrypt.hash_pwd_salt(login_name <> "123!")
+
+  insert!(%User{
+    login_name: login_name,
+    hashed_password: hashed_password
+  })
+end
