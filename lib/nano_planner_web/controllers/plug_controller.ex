0fa57580f720d96baca9476af2324640ed7a6a49
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-28 00:39:11 +0900

    [ex05/ch08] plug :set_number, min: 101, max: 200

--- lib/nano_planner_web/controllers/plug_controller.ex
+++ lib/nano_planner_web/controllers/plug_controller.ex
@@ -1,13 +1,15 @@
 defmodule NanoPlannerWeb.PlugController do
   use NanoPlannerWeb, :controller
 
-  plug :set_number
+  plug :set_number, min: 101, max: 200
 
   def show(conn, _params) do
     render(conn, "show.html")
   end
 
-  defp set_number(conn, _opts) do
-    assign(conn, :number, Enum.random(1..100))
+  defp set_number(conn, opts) do
+    min = opts[:min] || 1
+    max = opts[:max] || 100
+    assign(conn, :number, Enum.random(min..max))
   end
 end
