9963bb0431ec7375aac79610a953a05b08de6f64
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-29 02:38:00 +0900

    [ex05/08] UserSessionControllerTest

--- test/nano_planner_web/controllers/user_session_controller_test.exs
+++ test/nano_planner_web/controllers/user_session_controller_test.exs
@@ -1,10 +1,25 @@
 defmodule NanoPlannerWeb.UserSessionControllerTest do
   use NanoPlannerWeb.ConnCase, async: true
+  import NanoPlanner.AccountsFixtures
 
   describe "GET /users/log_in" do
+    setup do
+      user = user_fixture(login_name: "alice")
+      {:ok, user: user}
+    end
+
     test "ログインフォームを表示する", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
       assert conn.status == 200
+    end
+
+    test "トップページにリダイレクトする", %{conn: conn, user: user} do
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get(Routes.user_session_path(conn, :new))
+
+      assert redirected_to(conn) == "/"
     end
   end
 end
