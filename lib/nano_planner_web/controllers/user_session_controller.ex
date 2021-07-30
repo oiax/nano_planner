905363b017aa0ffa3693a0776270dfeaa090494e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-30 10:10:50 +0900

    [ex05/09] UserSesscionController.create/2

--- lib/nano_planner_web/controllers/user_session_controller.ex
+++ lib/nano_planner_web/controllers/user_session_controller.ex
@@ -1,12 +1,27 @@
 defmodule NanoPlannerWeb.UserSessionController do
   use NanoPlannerWeb, :controller
 
-  import NanoPlannerWeb.UserAuth,
-    only: [redirect_if_user_is_authenticated: 2]
-
-  plug :redirect_if_user_is_authenticated
-
   def new(conn, _params) do
     render(conn, "new.html")
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    user =
+      Accounts.get_user_by_login_name_and_password(
+        user_params["login_name"],
+        user_params["password"]
+      )
+
+    if user do
+      session_token = Accounts.generate_session_token(user)
+
+      conn
+      |> configure_session(renew: true)
+      |> clear_session()
+      |> put_session(:session_token, session_token)
+      |> redirect(to: Routes.top_path(conn, :index))
+    else
+      render(conn, "new.html")
+    end
   end
 end
--- test/nano_planner_web/controllers/user_session_cntlr_test.exs
+++ test/nano_planner_web/controllers/user_session_cntlr_test.exs
@@ -10,7 +10,50 @@ defmodule NanoPlannerWeb.UserSessionControllerTest do
 
     test "ログインフォームを表示する", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
-      assert conn.status == 200
+
+      assert Phoenix.Controller.view_template(conn) == "new.html"
+    end
+
+    test "トップページにリダイレクトする", %{conn: conn, user: user} do
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get(Routes.user_session_path(conn, :new))
+
+      assert redirected_to(conn) == "/"
+    end
+  end
+
+  describe "POST /users/log_in" do
+    setup do
+      user = user_fixture(login_name: "alice")
+      {:ok, user: user}
+    end
+
+    test "ログインに成功する", %{conn: conn, user: user} do
+      params = %{
+        "user" => %{
+          "login_name" => user.login_name,
+          "password" => user.login_name <> "123!"
+        }
+      }
+
+      conn = post(conn, Routes.user_session_path(conn, :create), params)
+
+      assert redirected_to(conn) == "/"
+    end
+
+    test "ログインに失敗する", %{conn: conn, user: user} do
+      params = %{
+        "user" => %{
+          "login_name" => user.login_name,
+          "password" => "oops!"
+        }
+      }
+
+      conn = post(conn, Routes.user_session_path(conn, :create), params)
+
+      assert Phoenix.Controller.view_template(conn) == "new.html"
     end
 
     test "トップページにリダイレクトする", %{conn: conn, user: user} do
