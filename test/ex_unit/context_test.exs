6ae735ff324e1013b9adb7a032a0f91c53e250b6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-07-31 19:08:17 +0900

    [ex05/07] ex_unit/context_test.exs (3)

--- test/ex_unit/context_test.exs
+++ test/ex_unit/context_test.exs
@@ -1,7 +1,5 @@
 defmodule NanoPlanner.ContextTest do
-  use NanoPlanner.DataCase
-  alias NanoPlanner.Accounts
-  import NanoPlanner.AccountsFixtures
+  use ExUnit.Case
 
   setup do
     {:ok, number: 100}
@@ -22,17 +20,5 @@ defmodule NanoPlanner.ContextTest do
   test "コンテキストを取得する", context do
     assert context[:number] == 100
     assert context[:message] == "Hello"
-  end
-
-  describe "Accounts.get_user_by_login_name_and_password/2" do
-    setup do
-      user_fixture(login_name: "taro")
-      :ok
-    end
-
-    test "ユーザーを返す" do
-      u = Accounts.get_user_by_login_name_and_password("taro", "taro123!")
-      assert %Accounts.User{} = u
-    end
   end
 end
