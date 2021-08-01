2adf26ac5fb47b3dcac97b1ae5ca26ada9382486
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-08-01 13:25:03 +0900

    [ex05/03] Fix priv/use_*.sh

--- priv/use_mariadb.sh
+++ priv/use_mariadb.sh
@@ -6,4 +6,5 @@ sed -i -e 's/hostname: "postgres"/hostname: "mariadb"/' config/dev.exs
 sed -i -e 's/username: "postgres"/username: "root"/' config/test.exs
 sed -i -e 's/password: "postgres"/password: "root"/' config/test.exs
 sed -i -e 's/hostname: "postgres"/hostname: "mariadb"/' config/test.exs
+mix deps.clean --all
 mix deps.get
--- priv/use_postgresql.sh
+++ priv/use_postgresql.sh
@@ -6,4 +6,5 @@ sed -i -e 's/hostname: "mariadb"/hostname: "postgres"/' config/dev.exs
 sed -i -e 's/username: "root"/username: "postgres"/' config/test.exs
 sed -i -e 's/password: "root"/password: "postgres"/' config/test.exs
 sed -i -e 's/hostname: "mariadb"/hostname: "postgres"/' config/test.exs
+mix deps.clean --all
 mix deps.get
