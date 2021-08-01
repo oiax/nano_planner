6a72b77a4add4a38e7b335a04146e1b3fd8bd4d5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-08-01 11:38:59 +0900

    [ex05] Improve plan_item_controller_test.exs

--- test/nano_planner_web/controllers/plan_item_controller_test.exs
+++ test/nano_planner_web/controllers/plan_item_controller_test.exs
@@ -13,16 +13,18 @@ defmodule NanoPlannerWeb.PlanItemControllerTest do
 
     @tag :login
     test "予定項目の一覧を表示する", %{conn: conn} do
-      conn = get(conn, Routes.plan_item_path(conn, :index))
-
-      plan_items = conn.assigns.plan_items
+      conn = get(conn, "/plan_items")
 
       assert conn.status == 200
+
+      plan_items = conn.assigns[:plan_items]
+
+      assert is_list(plan_items)
       assert length(plan_items) == 2
     end
 
     test "ログインフォームにリダイレクトする", %{conn: conn} do
-      conn = get(conn, Routes.plan_item_path(conn, :index))
+      conn = get(conn, "/plan_items")
 
       assert redirected_to(conn) == "/users/log_in"
     end
@@ -45,7 +47,7 @@ defmodule NanoPlannerWeb.PlanItemControllerTest do
         }
       }
 
-      conn = post(conn, Routes.plan_item_path(conn, :create), params)
+      conn = post(conn, "/plan_items", params)
 
       [item] = Repo.all(PlanItem)
 
