8ed22632ffb10add10c54a53e573dcc58c5111d6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-08-09 00:09:35 +0900

    [ex05/14] SessionToken benlogs to User

--- lib/nano_planner/accounts/session_token.ex
+++ lib/nano_planner/accounts/session_token.ex
@@ -1,8 +1,9 @@
 defmodule NanoPlanner.Accounts.SessionToken do
   use Ecto.Schema
+  alias NanoPlanner.Accounts.User
 
   schema "session_tokens" do
-    field :user_id, :integer
+    belongs_to :user, User
     field :token, :binary
 
     timestamps(type: :utc_datetime_usec, updated_at: false)
