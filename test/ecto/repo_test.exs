bc2d1bb0194f493d5235bfb3d89959838e65bcea
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-08-10 00:44:00 +0900

    [ex05/14] repo_test.exs

---
+++ test/ecto/repo_test.exs
@@ -0,0 +1,54 @@
+defmodule Ecto.RepoTest do
+  use NanoPlanner.DataCase
+  import NanoPlanner.AccountsFixtures
+  import NanoPlanner.ScheduleFixtures
+  alias NanoPlanner.Accounts.User
+  alias NanoPlanner.Schedule.PlanItem
+
+  setup do
+    {:ok, user: user_fixture()}
+  end
+
+  describe "preload/3" do
+    test "予定項目のownerをプリロードする", %{user: user} do
+      item = plan_item_fixture(owner: user)
+      fetched = Repo.get!(PlanItem, item.id)
+      loaded = Repo.preload(fetched, :owner)
+
+      assert %Ecto.Association.NotLoaded{} = fetched.owner
+      assert %User{} = loaded.owner
+    end
+  end
+
+  describe ":preload キー" do
+    test "予定項目のownerをプリロードする", %{user: user} do
+      item = plan_item_fixture(owner: user)
+
+      query =
+        from i in PlanItem,
+          where: i.id == ^item.id,
+          preload: :owner
+
+      fetched = Repo.one!(query)
+
+      assert %User{} = fetched.owner
+    end
+  end
+
+  describe "テーブル結合" do
+    test "予定項目のownerをプリロードする", %{user: user} do
+      item = plan_item_fixture(owner: user)
+
+      query =
+        from i in PlanItem,
+          where: i.id == ^item.id,
+          join: u in User,
+          on: i.owner_id == u.id,
+          select: %{i | owner: u}
+
+      fetched = Repo.one!(query)
+
+      assert %User{} = fetched.owner
+    end
+  end
+end
